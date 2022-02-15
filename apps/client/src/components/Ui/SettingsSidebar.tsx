8fababaebaaea27795916f5cb17ea7998123bbf9
Juul Crooymans <juul.crooymans@gmail.com>
2022-02-15 19:04:45 +0100

    responsive settings

--- apps/client/src/components/Ui/SettingsSidebar.tsx
+++ apps/client/src/components/Ui/SettingsSidebar.tsx
@@ -22,30 +22,48 @@ function SettingsSidebar() {
     <div className="flex justify-end">
       <div className="w-full flex flex-col">
         {isLoading ? (
-          <div className="pl-2 mb-5 flex flex-col">
+          <div className="lg:pl-2 mb-5 flex flex-col">
             <div className="bg-gray-100 dark:bg-dark-500 animate-pulse h-6 rounded-md w-full mb-1"></div>
             <div className="bg-gray-100 dark:bg-dark-500 animate-pulse h-4 rounded w-1/2"></div>
           </div>
         ) : (
-          <div className="pl-2 mb-6 flex flex-col">
+          <div className="lg:pl-2 mb-6 flex flex-col">
             <span className="h-6 font-semibold truncate">{data.me.email}</span>
             <span className="text-sm dark:text-text-dark-200 text-text-200 h-4">
               Hobby
             </span>
           </div>
         )}
-        {routes.map((r) => (
-          <Link key={r.route} href={r.route}>
-            <a
-              className={cn(
-                "p-2 rounded-md mb-1 text-sm",
-                r.route === pathname ? "dark:bg-dark-500 bg-gray-100" : ""
-              )}
-            >
-              {r.name}
-            </a>
-          </Link>
-        ))}
+        <div className="flex justify-between lg:hidden border-b dark:border-border-dark border-border-light mb-4">
+          {routes.map((r) => (
+            <Link key={r.route} href={r.route}>
+              <a
+                className={cn(
+                  "px-4 pb-2",
+                  r.route === pathname
+                    ? "dark:border-white border-dark border-b"
+                    : ""
+                )}
+              >
+                {r.name}
+              </a>
+            </Link>
+          ))}
+        </div>
+        <div className="hidden lg:flex flex-col">
+          {routes.map((r) => (
+            <Link key={r.route} href={r.route}>
+              <a
+                className={cn(
+                  "p-2 rounded-md mb-1 text-sm",
+                  r.route === pathname ? "dark:bg-dark-500 bg-gray-100" : ""
+                )}
+              >
+                {r.name}
+              </a>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
